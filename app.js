cb4ef90d5d803c8c58e7337481f3356663b2acf2
OsmanliFarid <osmanliferid686@gmail.com>
2025-04-03 23:10:53 +0400

    cg

--- app.js
+++ app.js
@@ -3,18 +3,17 @@
 const body = document.body; // Body elementini seçirik
 const newBox = document.getElementById("newBox"); // 'newBox' elementini seçirik
 const newBox22 = document.getElementById("newBox22"); // 'newBox22' elementini seçirik
-const newBox3 = document.getElementById("newBox3"); // 'newBox3' elementini seçirik
+const newBox3 = document.getElementById("newBox3"); 
 let indek = 0
 function newBox2() {
-    // Düymələrin fon rəngini təyin edirik
-    if (body.style.backgroundColor === "") { // başlanğıcda ağ
-        body.style.backgroundColor = "blue"; // 'newBox' mavi fon
+    if (body.style.backgroundColor === "") { 
+        body.style.backgroundColor = "blue";
     } else if (body.style.backgroundColor === "blue") { 
-        body.style.backgroundColor = "red"; // 'newBox22' qırmızı fon
+        body.style.backgroundColor = "red"; 
     } else if (body.style.backgroundColor === "red") {
-        body.style.backgroundColor = "green"; // 'newBox3' yaşıl fon
+        body.style.backgroundColor = "green"; 
     } else {
-        body.style.backgroundColor = "blue"; // Yenidən mavi fon
+        body.style.backgroundColor = "blue"; 
     }
     if(indek >= a)
 }
